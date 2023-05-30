588986e68f0fde673319a72b2a6bd0ecf793eb3a
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-05-30 19:48:12 +0200

    UI updates

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,36 +1,44 @@
-import React from 'react'
-import { Box, Button, Typography } from '@mui/material'
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
 
-import heroImage from '../assets/images/hero-image.jpg'
+import heroImage from "../assets/images/hero-image.jpg";
 
 const Hero = () => {
   return (
-    <Box sx={{ position: 'relative', mb: '70px' }} >
-      <img src={heroImage} className='hero-image' alt='background' />
-      <Box 
-        sx={{ 
-          position: {lg: "absolute", xs: "unset"}, 
-          top: {lg: '40%', xs: ''}, 
-          left: {lg: '5%', xs: ''}, 
-          display: 'flex', 
-          flexDirection: 'column', 
-          alignItems: 'center', 
-          textAlign: 'center'
-          }}
+    <Box sx={{ position: "relative", mb: "70px" }}>
+      <img src={heroImage} className="hero-image" alt="background" />
+      <Box
+        sx={{
+          position: { lg: "absolute", xs: "unset" },
+          top: { lg: "40%", xs: "" },
+          left: { lg: "5%", xs: "" },
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <Typography
+          variant="h2"
+          fontWeight="600"
+          mb="20px"
+          mx="20px"
+          sx={{ mt: { lg: "auto", xs: "50px" } }}
         >
-        <Typography 
-          variant='h2'
-          fontWeight='600' 
-          mb='20px' 
-          mx='20px' 
-          sx={{ mt:{ lg: 'auto', xs: '50px' }}}
-        >
-          Find delicious meal for today!
+          Find delicious meal <br /> for today!
         </Typography>
-        <Button variant='contained' color='success' size='large' sx={{ mt: { lg: 'auto', xs: '30px' } }} href='#meals'>Check Meals</Button>
+        <Button
+          variant="contained"
+          color="success"
+          size="large"
+          sx={{ mt: { lg: "auto", xs: "30px" } }}
+          href="#meals"
+        >
+          Check Meals
+        </Button>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/pages/Meal.js
+++ src/pages/Meal.js
@@ -1,103 +1,145 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { Stack, Typography, Box, Chip } from '@mui/material'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Stack, Typography, Box, Chip } from "@mui/material";
 
-import { fetchData, mealOptions } from '../utils/fetchData'
+import { fetchData, mealOptions } from "../utils/fetchData";
 
 const Meal = () => {
-  const [details, setDetails] = useState({})
-  const { id } = useParams()
-  const paragraphLength = 300
+  const [details, setDetails] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+  const paragraphLength = 300;
 
-  useEffect(() => { 
+  useEffect(() => {
     const fetchMealDetails = async () => {
-      const mealDetails = await fetchData(`https://themealdb.p.rapidapi.com/lookup.php?i=${id}`, mealOptions)
-      setDetails(mealDetails.meals[0])
-    }
-    fetchMealDetails()
-  }, [id])
+      const mealDetails = await fetchData(
+        `https://themealdb.p.rapidapi.com/lookup.php?i=${id}`,
+        mealOptions
+      );
+      setDetails(mealDetails.meals[0]);
+      setIsLoading(false);
+    };
+    fetchMealDetails();
+  }, [id]);
 
-  const ingredientsKeys = Object.keys(details).filter(key => 
-    key.includes('strIngredient') && details[key] !== "" && details[key] !== null
-  )
-  const ingredients = ingredientsKeys.map(key => {
+  const ingredientsKeys = Object.keys(details).filter(
+    (key) =>
+      key.includes("strIngredient") &&
+      details[key] !== "" &&
+      details[key] !== null
+  );
+  const ingredients = ingredientsKeys.map((key) => {
     return (
-      <Typography key={key} variant='h6' >
+      <Typography key={key} variant="h6">
         <li>{details[key]}</li>
       </Typography>
-    )
-  })
+    );
+  });
 
-  const mealTags = details.strTags?.split(',').map(tag => {
+  const mealTags = details.strTags?.split(",").map((tag) => {
     return (
-      <Chip label={tag} color='success' variant='filled' key={tag} sx={{ fontWeight: '400', fontSize: '14px', color: 'white', letterSpacing: '0.05em'}} />
-    )
-  })
+      <Chip
+        label={tag}
+        color="success"
+        variant="filled"
+        key={tag}
+        sx={{
+          fontWeight: "400",
+          fontSize: "14px",
+          color: "white",
+          letterSpacing: "0.05em",
+        }}
+      />
+    );
+  });
 
   // formatting plain text
-  const sentences = details.strInstructions?.split('.').map(sentence => sentence + '.')
-  let paragraphs = []
-  if(sentences) {
-      for (let i = 0; i < sentences.length; i++) {
-        if(i === 0) {
-          paragraphs[0] = sentences[i]
-        } else if(paragraphs[paragraphs.length - 1].length + sentences[i].length < paragraphLength) {
-          paragraphs[paragraphs.length - 1] = paragraphs[paragraphs.length - 1] + sentences[i]
-        } else {
-          paragraphs[paragraphs.length] = sentences[i]
-        }
+  const sentences = details.strInstructions
+    ?.split(".")
+    .map((sentence) => sentence + ".");
+  let paragraphs = [];
+  if (sentences) {
+    for (let i = 0; i < sentences.length; i++) {
+      if (i === 0) {
+        paragraphs[0] = sentences[i];
+      } else if (
+        paragraphs[paragraphs.length - 1].length + sentences[i].length <
+        paragraphLength
+      ) {
+        paragraphs[paragraphs.length - 1] =
+          paragraphs[paragraphs.length - 1] + sentences[i];
+      } else {
+        paragraphs[paragraphs.length] = sentences[i];
       }
+    }
   }
 
   const instructions = paragraphs.map((sentence, index) => {
-    return(
-      <Stack key={index} spacing={2} mt='10px' >
-        <Typography fontSize='1.5em' fontWeight='500' >
+    return (
+      <Stack key={index} spacing={2} mt="10px">
+        <Typography fontSize="1.5em" fontWeight="500">
           {`Step ${index + 1}`}
         </Typography>
-        <Typography fontSize='1em' letterSpacing='0.03em' lineHeight='1.5em' >
+        <Typography fontSize="1em" letterSpacing="0.03em" lineHeight="1.5em">
           {sentence}
         </Typography>
       </Stack>
-    )
-  })
+    );
+  });
 
-  return (
-    <Stack alignItems='center' mt='50px' spacing={7}  >
-      <Stack direction='row' spacing={8} sx={{ flexDirection: { md: 'row', xs: 'column' } }} >
-        <Box 
-          mx='auto'
-          sx={{ 
-            width: { xl: '700px', lg: '550px', md: '450px', sm: '80%', xs: '90%' },
-            height: 'auto'
-            }} >
-          <img src={details.strMealThumb} alt={details.strMeal} className='meal-page-image' />
+  return isLoading ? (
+    <div className="loading-container">
+      <h2>Loading...</h2>
+    </div>
+  ) : (
+    <Stack alignItems="center" mt="50px" spacing={7}>
+      <Stack
+        direction="row"
+        spacing={8}
+        sx={{ flexDirection: { md: "row", xs: "column" } }}
+      >
+        <Box
+          mx="auto"
+          sx={{
+            width: {
+              xl: "700px",
+              lg: "550px",
+              md: "450px",
+              sm: "80%",
+              xs: "90%",
+            },
+            height: "auto",
+          }}
+        >
+          <img
+            src={details.strMealThumb}
+            alt={details.strMeal}
+            className="meal-page-image"
+          />
         </Box>
         <Stack spacing={3}>
-          <Typography variant='h3' sx={{ marginTop: { md: '0', xs: '30px' } }} >
+          <Typography variant="h3" sx={{ marginTop: { md: "0", xs: "30px" } }}>
             {details.strMeal}
           </Typography>
-          <Stack direction='row' flexWrap='wrap' mb='20px' spacing={1}>
+          <Stack direction="row" flexWrap="wrap" mb="20px" spacing={1}>
             {mealTags && mealTags}
           </Stack>
-          <Typography variant='h5' sx={{ textDecoration: 'underline' }} >
+          <Typography variant="h5" sx={{ textDecoration: "underline" }}>
             Ingredients:
           </Typography>
-          <Stack pl='15px' >
-            {ingredients}
-          </Stack>
+          <Stack pl="15px">{ingredients}</Stack>
         </Stack>
       </Stack>
-      <Box px='40px' maxWidth='800px' >
+      <Box px="40px" maxWidth="800px">
         <Stack spacing={3}>
-          <Typography variant='h5' sx={{ textDecoration: 'underline' }} >
+          <Typography variant="h5" sx={{ textDecoration: "underline" }}>
             Preparation:
           </Typography>
           {instructions}
         </Stack>
       </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default Meal
+export default Meal;
